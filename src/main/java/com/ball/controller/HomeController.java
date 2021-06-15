1bd9f9b4f7b5d7c308a96c7deeb87b91fa1fd43b
tyang0123 <84624287+tyang0123@users.noreply.github.com>
2021-06-15 10:10:21 +0900

    0615 1009

--- src/main/java/com/ball/controller/HomeController.java
+++ src/main/java/com/ball/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.ball.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,9 @@ public class HomeController {
         return "sample";
     }
 
+    @GetMapping("/create")
+    public String create(){
+        return "user/create";
+    }
 
 }
---
+++ src/main/webapp/WEB-INF/views/user/create.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html>
+<head>
+    <title>회원가입 페이지</title>
+</head>
+<body>
+  회원가입 페이지
+</body>
+</html>
+
+<%@ include file="../includes/footer.jsp" %>
